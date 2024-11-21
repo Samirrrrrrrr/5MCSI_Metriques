c285680dcde28e1f80fd79c14136ed7f07ad9ba6
Samirrrrrrrr <93645786+Samirrrrrrrr@users.noreply.github.com>
2024-11-21 16:44:55 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -36,6 +36,30 @@ def commits():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+@app.route('/tawarano/')
+def meteo():
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
+
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
+
+@app.route("/contact/")
+def MaPremiereAPI():
+    return "<h2>Ma page de contact</h2>"
+
+@app.route('/histogramme/')
+def histogramme():
+    return render_template("histogramme.html")
+
 @app.route('/')
 def home():
     return render_template('hello.html')
