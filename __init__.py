6fa02646604bc7985f7bb1c6e323ff72dc7022ea
Samirrrrrrrr <93645786+Samirrrrrrrr@users.noreply.github.com>
2024-11-21 16:33:25 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -36,5 +36,9 @@ def commits():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+@app.route('/')
+def home():
+    return render_template('hello.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
